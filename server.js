12fe6d35c7824936e2ea742fb8d6a820a832ba59
GamjaJEON <indian_potato@naver.com>
2024-03-29 01:23:36 +0900

    feat: res.json() vs res.send()

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 
 const PORT = 8000;
-const HOST = '0.0.0.0';
+
+const Users = [
+  { id: 0, name: 'Jack' },
+  { id: 1, name: 'Jsnnifer' },
+];
 
 const app = express();
+
+app.get('/users', (req, res) => {
+  res.send(Users);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/users/:userID', (req, res) => {
+  const userId = Number(req.params.userId);
+  const user = Users[userId];
+  if (user) {
+    res.json(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
