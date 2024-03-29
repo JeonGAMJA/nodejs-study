2710a4781fb9bcfc9c0e2e92ef7da31d145a7085
GamjaJEON <indian_potato@naver.com>
2024-03-29 10:35:46 +0900

    feat: POST 요청하기

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const Users = [
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   const start = Data.now();
   console.log(`start: ${req.method}${req.url}`);
@@ -17,12 +19,28 @@ app.use((req, res, next) => {
   console.log(`end: ${req.method}${req.url}${diffTime}ms`);
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
 app.get('/users', (req, res) => {
   res.send(Users);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.post('/users', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing user name',
+    });
+  }
+
+  console.log('req.body.name:' + req.body.name);
+  const newUser = {
+    name: req.body.name,
+    id: Users.length,
+  };
+  Users.push(newUser);
+  res.json(newUser);
 });
 
 app.get('/users/:userID', (req, res) => {
