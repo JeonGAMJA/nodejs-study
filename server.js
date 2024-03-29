9845afd1aef2a00236fe5378438fe8d71df5b143
GamjaJEON <indian_potato@naver.com>
2024-03-29 11:35:56 +0900

    feat: Router,Controller

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const usersController = require('./controllers/users.controller');
-const postController = require('./controllers/posts.controller');
-
 const PORT = 8000;
+
+const usersRouter = require('./routes/users.router');
+const postsRouter = require('./routes/posts.router');
 
 const app = express();
 
@@ -14,18 +14,15 @@ app.use((req, res, next) => {
   console.log(`start: ${req.method}${req.url}`);
   next();
   const diffTime = Date.now() - start;
-  console.log(`end: ${req.method}${req.url}${diffTime}ms`);
+  console.log(`end: ${req.method}${req.baseUrl}${req.url}${diffTime}ms`);
 });
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.get('/users', usersController.getUsers);
-app.get('/users/:userID', usersController.getUser);
-app.post('/users', usersController.postUser);
-
-app.get('/posts', postController.getPost);
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 app.listen(PORT);
 console.log(`Running on http://localhost:${PORT}`);
