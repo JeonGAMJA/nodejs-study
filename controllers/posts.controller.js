71b41c6759b3ff87593ef207097f0c5f6027bc16
GamjaJEON <indian_potato@naver.com>
2024-03-29 12:07:02 +0900

    feat: Template Layout 생성하기

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
 function getPost(req, res) {
-  res.sendFile(
-    path.join(
-      __dirname,
-      '..',
-      'public',
-      'images',
-      '스크린샷 2024-03-26 오전 4.24.52.png',
-    ),
-  );
+  res.render('posts', {
+    templateName: 'post',
+  });
+  //   res.sendFile(
+  //     path.join(
+  //       __dirname,
+  //       '..',
+  //       'public',
+  //       'images',
+  //       '스크린샷 2024-03-26 오전 4.24.52.png',
+  //     ),
+  //   );
   //   res.send('<div><h1>Post Title</h1><p>This is a post</p>>/div>');
 }
 
