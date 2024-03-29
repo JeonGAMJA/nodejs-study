a01211494d7f0ef52a4640eee306855e50771954
GamjaJEON <indian_potato@naver.com>
2024-03-29 11:44:44 +0900

    feat: express.static()

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 function getPost(req, res) {
-  res.send('<div><h1>Post Title</h1><p>This is a post</p>>/div>');
+  res.sendFile(
+    path.join(
+      __dirname,
+      '..',
+      'public',
+      'images',
+      '스크린샷 2024-03-26 오전 4.24.52.png',
+    ),
+  );
+  //   res.send('<div><h1>Post Title</h1><p>This is a post</p>>/div>');
 }
 
 module.exports = {
